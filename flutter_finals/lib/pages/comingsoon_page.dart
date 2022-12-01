c7802ede7ae8240074451604888fcb7b31d9d7ce
Kyle <kyle_sanchez99@yahoo.com>
2022-12-01 17:20:57 +0800

    Building initial structure for coming soon page

---
+++ flutter_finals/lib/pages/comingsoon_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ComingSoonPage extends StatefulWidget {
+  const ComingSoonPage({super.key});
+
+  @override
+  State<ComingSoonPage> createState() => _ComingSoonPageState();
+}
+
+class _ComingSoonPageState extends State<ComingSoonPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+    );
+  }
+}
