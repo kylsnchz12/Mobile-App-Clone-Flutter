1d91a41d88b35227d2d287c2d9b30418175b0dac
Kyle <kyle_sanchez99@yahoo.com>
2022-12-03 17:45:07 +0800

    Added show cards for top searches

---
+++ flutter_finals/lib/json/search_json.dart
@@ -0,0 +1,38 @@
+const List searchJson = [
+  {
+    "img": "assets/images/search_1.png",
+    "title": "Stranger Things 4",
+  },
+  {
+    "img": "assets/images/search_2.png",
+    "title": "The Witcher",
+  },
+  {
+    "img": "assets/images/search_3.png",
+    "title": "Shadow and Bone",
+  },
+  {
+    "img": "assets/images/search_4.png",
+    "title": "The Adam Project",
+  },
+  {
+    "img": "assets/images/search_5.png",
+    "title": "Dahmer",
+  },
+  {
+    "img": "assets/images/search_6.png",
+    "title": "Enola Homes 2",
+  },
+  {
+    "img": "assets/images/search_7.png",
+    "title": "The Sea Beast",
+  },
+  {
+    "img": "assets/images/search_8.png",
+    "title": "Red Notice",
+  },
+  {
+    "img": "assets/images/search_9.png",
+    "title": "Dash and Lily",
+  },
+];
--- flutter_finals/lib/pages/search_page.dart
+++ flutter_finals/lib/pages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_finals/json/search_json.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -14,6 +15,7 @@ class _SearchPageState extends State<SearchPage> {
       backgroundColor: Colors.black,
       appBar: PreferredSize(
           preferredSize: const Size.fromHeight(55), child: getAppBar()),
+      body: getBody(),
     );
   }
 
@@ -37,6 +39,96 @@ class _SearchPageState extends State<SearchPage> {
                 Icons.search,
                 color: Colors.white.withOpacity(0.5),
               )),
+        ),
+      ),
+    );
+  }
+
+  Widget getBody() {
+    var size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10, left: 18, right: 18),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Top Searches",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18),
+            ),
+            const SizedBox(height: 12),
+            Column(
+                children: List.generate(searchJson.length, (index) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: (size.width - 36) * 0.8,
+                      height: 80,
+                      child: Row(
+                        children: [
+                          Stack(
+                            children: [
+                              Container(
+                                width: 120,
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            searchJson[index]['img']),
+                                        fit: BoxFit.cover)),
+                              ),
+                              Container(
+                                width: 120,
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    color: Colors.black.withOpacity(0.2)),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(width: 10),
+                          SizedBox(
+                            width: (size.width - 36) * 0.4,
+                            child: Text(
+                              searchJson[index]['title'],
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                        width: (size.width - 36) * 0.2,
+                        height: 80,
+                        child: Center(
+                          child: Container(
+                            width: 35,
+                            height: 35,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border:
+                                    Border.all(width: 2, color: Colors.white)),
+                            child: const Center(
+                              child: Icon(
+                                Icons.play_arrow,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ))
+                  ],
+                ),
+              );
+            }))
+          ],
         ),
       ),
     );
